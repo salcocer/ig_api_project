cb278e4ccd0d973b0559a9964276c41cf073d019
Alejandro Alcocer <saavalencia97@gmail.com>
2026-03-08 18:29:08 +0700

    webhook

--- chat/hooks/useSocket.ts
+++ chat/hooks/useSocket.ts
@@ -6,8 +6,9 @@ let sharedSocket: Socket | null = null;
 export function initSocket(url?: string) {
     if (typeof window === 'undefined') return null;
     if (!sharedSocket) {
-        const defaultOrigin =
-            typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000';
+        // const defaultOrigin =
+        // typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000';
+        const defaultOrigin = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3000';
         const target = url || defaultOrigin;
         sharedSocket = io(target, { transports: ['websocket', 'polling'] });
     }
